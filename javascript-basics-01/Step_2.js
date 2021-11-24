caf9309e063796a430365c499315e5d636022e48
Nourcova <ibrahamnour22@gmail.com>
2021-11-24 21:35:24 +0200

    Step_2 basics 1 Completed

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,7 @@
+const showInfo = ()=>{
+   let name="Doe";
+   let surname = "John";
+   let city = "New-York";
+   alert ("Nom : " + name + "\nPrénom : " + surname + "\nVille : " + city);
+}
+showInfo();
