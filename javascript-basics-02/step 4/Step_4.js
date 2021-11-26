716d7204dd54bac14d24304ea85a1242280ce1e8
Nourcova <ibrahamnour22@gmail.com>
2021-11-26 10:01:13 +0200

    step 4 basics 2 Completed

--- javascript-basics-02/step 4/Step_4.js
+++ javascript-basics-02/step 4/Step_4.js
@@ -1,6 +1,9 @@
 const clearFields = () => {
-    let values = document.getElementsByTagName("input")
-    for (let i = 0; i < values.length; i++) {
-        values[i].value = "";
+    let answer = confirm("Do you want to reset all the fields?")
+    if (answer) {
+        let values = document.getElementsByTagName("input")
+        for (let i = 0; i < values.length; i++) {
+            values[i].value = "";
+        }
     }
 }
